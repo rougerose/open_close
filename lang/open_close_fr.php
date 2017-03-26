00aaa4cfc250bd2443b011b3e73db2a388e543f3
christophe le drean <christophe@rouge-rose.net>
2017-03-26 19:05:57 +0200

    Ajout chaîne manquante

--- lang/open_close_fr.php
+++ lang/open_close_fr.php
@@ -9,6 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']]  = array(
     2- pour des horaires avec interruption : <code>HH:mm-HH:mm, HH:mm-HH:mm</code><br> Exemple : <code>09:00-13:00, 14:00-19:00</code>\n
     3- pour les jours de fermeture : le champs doit rester vide.",
   'config_info_horaires_resume'      => 'Les horaires saisis ci-dessous seront affichés tels quels sur les pages publiques du site',
+  'config_info_vacances' => 'Les prochaines dates de vacances. La saisie est de type <code>JJ/MM/AAAA</code>',
   'config_label_jour1'               => 'Lundi',
   'config_label_jour2'               => 'Mardi',
   'config_label_jour3'               => 'Mercredi',
