ba1cf51e58f5f064f2fe6ade5c41f93909ade00b
christophe le drean <christophe@rouge-rose.net>
2017-03-28 01:04:38 +0200

    Dates et chaîne relatives aux vacances
    Ajouter un troisième argument (chaîne relative aux vacances)
    et, dans l'hypothèse où la date de fin des vacances est à venir,
    afficher le message correspondant.

--- open_close_fonctions.php
+++ open_close_fonctions.php
@@ -14,24 +14,24 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * si le lieu est ouvert ou fermé
  *
  * @param date $date La date passée en argument n'est pas prise en compte.
- * @param string $ouvert La chaîne à retourner si le lieu est ouvert.
- * @param string $ferme La chaîne à retourner si le lieu est fermé.
+ * @param string $msg_ouvert La chaîne à retourner si le lieu est ouvert.
+ * @param string $msg_ferme La chaîne à retourner si le lieu est fermé.
+ * @param string $msg_vacances La chaîne à retourner si le lieu est en vacances.
  * @return return string
  */
-function filtre_isOpenClose($date, $ouvert, $ferme) {
+function filtre_isOpenClose($date, $msg_ouvert, $msg_ferme, $msg_vacances) {
 
   include_spip('lib/php-store-hours/StoreHours.class');
 
   $tz = lire_config('timezone');
   $oc = lire_config('open_close');
 
-  if (isset($tz) AND isset($oc)) {
+  if ( isset($tz) AND isset($oc) ) {
     date_default_timezone_set($tz);
 
-    $horaires = array();
-    $vacances = array();
-    $exceptions = array();
     $semaine = array('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun');
+
+    $message = $msg_ferme;
 
     foreach ($oc as $cle => $valeur) {
       // horaires
@@ -42,8 +42,8 @@ function filtre_isOpenClose($date, $ouvert, $ferme) {
       }
 
       // vacances
-      if (strstr($cle, 'vacances_') AND !empty($valeur)) {
-        // vacances_debut ou vacances_fin
+      if ( strstr($cle, 'vacances_') AND !empty($valeur) ) {
+        // vacances_debut et vacances_fin
         $periodes = substr($cle, 9);
 
         // convertir la date jj/mm/aaaa en Y-m-d
@@ -52,12 +52,18 @@ function filtre_isOpenClose($date, $ouvert, $ferme) {
       }
     }
 
-    if (count($vacances) == 2) {
+    if ( count($vacances) == 2 ) {
       $debut = new DateTime($vacances['debut']);
       $fin = new DateTime($vacances['fin']);
       $fin = $fin->modify('+1 day');
       $interval = new DateInterval('P1D');
       $dates = new DatePeriod($debut, $interval, $fin);
+
+      if ( agenda_date_a_venir($vacances['fin']) ) {
+        $debut_jour_court = affdate_jourcourt($vacances['debut']);
+        $fin_jour_court = affdate_jourcourt($vacances['fin']);
+        $message = _L($msg_vacances, array('date_debut' => $debut_jour_court, 'date_fin' => $fin_jour_court));
+      }
 
       foreach($dates as $date) {
         $cle = $date->format('Y-m-d');
@@ -67,13 +73,19 @@ function filtre_isOpenClose($date, $ouvert, $ferme) {
 
     $store_hours = new StoreHours($horaires, $exceptions);
 
-    if($store_hours->is_open()) {
-      return $ouvert;
+    if( $store_hours->is_open() ) {
+
+      return $msg_ouvert;
+
     } else {
-      return $ferme;
+
+      return $message;
+
     }
 
   } else {
+
     return '';
+
   }
 }
