4c2394ade505888f4e9d181062c6c2c06742bac7
christophe le drean <christophe@rouge-rose.net>
2017-03-28 02:27:02 +0200

    Vérifier date_fin
    Il ne semble pas possible de faire deux vérifications sur une
    saisie. La fonction de comparaison sert à faire les deux
    vérification (date postérieure et format de saisie).

--- verifier/comparaison_date.php
+++ verifier/comparaison_date.php
@@ -6,18 +6,25 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 function verifier_comparaison_date_dist($valeur, $options = array()) {
 
-  if (!$champ = $options['champ'] or !is_scalar($champ)) {
-    return true;
+  $verifier = charger_fonction('verifier', 'inc/');
+  $verifier_date = $verifier($valeur, 'date');
+  // var_dump($verifier_date);
+  if ($verifier_date) {
+    return $verifier_date;
   } else {
-    $valeur_champ = _request($champ);
-  }
+    if (!$champ = $options['champ'] or !is_scalar($champ)) {
+      return true;
+    } else {
+      $valeur_champ = _request($champ);
+    }
 
-  $date_debut = strtotime(date_format(date_create_from_format('j/m/Y', $valeur_champ), 'Y-m-d'));
-  $date_fin = strtotime(date_format(date_create_from_format('j/m/Y', $valeur), 'Y-m-d'));
+    $date_debut = strtotime(date_format(date_create_from_format('j/m/Y', $valeur_champ), 'Y-m-d'));
+    $date_fin = strtotime(date_format(date_create_from_format('j/m/Y', $valeur), 'Y-m-d'));
 
-  if ($date_fin < $date_debut) {
-    return _T('agenda:erreur_date_avant_apres');
-  } else {
-    return '';
+    if ($date_fin < $date_debut) {
+      return _T('agenda:erreur_date_avant_apres');
+    } else {
+      return '';
+    }
   }
 }
